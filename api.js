3897d54e51f2054c9321ddbd53560219f23d8580
Daniel Kristensen <dgkristensen@gmail.com>
2016-01-21 16:54:50 +1100

    initial commit

---
+++ api.js
@@ -0,0 +1,53 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "B2Joint",
+        "b2AreaJoint",
+        "b2AreaJointDef",
+        "b2Body",
+        "b2DistanceJoint",
+        "b2DistanceJointDef",
+        "b2FrictionJoint",
+        "b2FrictionJointDef",
+        "b2GearJoint",
+        "b2GearJointDef",
+        "b2Jacobian",
+        "b2JointDef",
+        "b2JointEdge",
+        "b2JointFactory",
+        "b2MotorJoint",
+        "b2MotorJointDef",
+        "b2MouseJoint",
+        "b2MouseJointDef",
+        "b2PrismaticJoint",
+        "b2PrismaticJointDef",
+        "b2PulleyJoint",
+        "b2PulleyJointDef",
+        "b2RevoluteJoint",
+        "b2RevoluteJointDef",
+        "b2RopeJoint",
+        "b2RopeJointDef",
+        "b2WeldJoint",
+        "b2WeldJointDef",
+        "b2WheelJoint",
+        "b2WheelJointDef",
+        "b2World"
+    ],
+    "modules": [
+        "Dynamics",
+        "Joints"
+    ],
+    "allModules": [
+        {
+            "displayName": "Dynamics",
+            "name": "Dynamics",
+            "description": "A rigid body. These are created via\n<a href=../classes/b2World.html#method_createBody>b2World.createBody()</a>."
+        },
+        {
+            "displayName": "Joints",
+            "name": "Joints",
+            "description": "Manages the operation of joints and their interaction with bodies.."
+        }
+    ]
+} };
+});
